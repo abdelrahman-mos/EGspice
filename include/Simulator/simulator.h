9ba96830c31065bf84177d467a19c59468fe0ccf
Abdelrahman Mostafa <boboelahlawy1234@hotmail.com>
2025-10-04 23:36:07 +0300

    adding basic skeleton

---
+++ include/Simulator/simulator.h
@@ -0,0 +1,6 @@
+#ifndef SIMULATOR_H
+#define SIMULATOR_H
+
+void run_simulator(char* netlist_path);
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,7 +1,7 @@
 #include "../include/utils/map.h"
 #include "../include/utils/strutils.h"
-#include "../include/Parser/SPICE/spiceParser.h"
 #include "../include/utils/MxAlg.h"
+#include "../include/Simulator/simulator.h"
 
 // int cmp_func(const void* a, const void* b) {
 //     return strcmp((const char *) a, (const char *) b);
@@ -29,32 +29,9 @@ int main(int argc, char *argv[]) {
     // printf("age: %d\n", *(int*) hashmap_get(my_map, "age"));
 
     // hashmap_destroy(my_map);
-
-    Netlist* parsed_netlist = parse_netlist(argv[1]);
-    char* analysis = (char*)hashmap_get(parsed_netlist->analyses, "an1");
-    printf("analysis: %s\n", analysis);
-    char** devices_names = hashmap_keys(parsed_netlist->devices);
-    for (int i = 0; devices_names[i] != NULL; i++) {
-        char* device_name = devices_names[i];
-        Device* device = hashmap_get(parsed_netlist->devices, device_name);
-        if (device->type == VSOURCE) {
-            Vsource* device_data = (Vsource*) device->device_data;
-            print_device(device_data);
-        } else if (device->type == ISOURCE)
-        {
-            Isource* device_data = (Isource*) device->device_data;
-            print_device(device_data);
-        } else if (device->type == RESISTOR)
-        {
-            Resistor* device_data = (Resistor*) device->device_data;
-            print_device(device_data);
-        }        
-    }
-    for (int i = 0; parsed_netlist->nodes[i] != NULL; i++) {
-        printf("node num %d corresponds to node name %s\n", i, parsed_netlist->nodes[i]);
-    }
-    free_parser(parsed_netlist);
-
+    char* netlist_path = argv[1];
+    run_simulator(netlist_path);
+    
     // char* line = "v1 va 0 32v";
     // char** line_split = splittext(line, " ");
     // for (int i = 0; line_split[i] != NULL; i++) {
